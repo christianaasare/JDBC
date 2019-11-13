32d65a62e832396a632848855f18c9a19cac4882
Christiana <kristiana.095@gmail.com>
2019-11-13 23:42:10 +0000

    JDBC and Postgres

--- src/main/java/io/turntabl/DBConnection.java
+++ src/main/java/io/turntabl/DBConnection.java
@@ -6,27 +6,39 @@ import java.sql.*;
 public class DBConnection {
     public static void DBConnection() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
-
         String url = "jdbc:postgresql:northwind";
-        System.out.println("---------------------------------------------------------------------------------------------");
-        System.out.printf("%5s %20s %15s %20s ", "Customer_ID", "Company_Name","Contact_Title", "City");
-        System.out.println();
-        System.out.println("---------------------------------------------------------------------------------------------");
-
-
-        try (Connection db = DriverManager.getConnection(url,"christiana-asare","turntabl")){
-            Statement s= db.createStatement();
-            ResultSet resultSet = s.executeQuery("select * from customers;");
-                System.out.printf("%5s %20s %15s %20s",resultSet.getString("customer_id"),resultSet.getString("company_name"),resultSet.getString("contact_title"), resultSet.getString("city"));
-            while (resultSet.next())
-
-
-            System.out.println("---------------------------------------------------------------------------------------------");
+        try(Connection db = DriverManager.getConnection(url,"christiana-asare","turntabl")) {
+            Statement s = db.createStatement();
+            ResultSet rs = s.executeQuery("select * from customers;");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.printf("%5s %12s %12s %8s", "Customer_ID", "Contact_Name",  "City", "Telephone");
             System.out.println();
-            System.out.println();
+            System.out.println("---------------------------------------------------------------------------");
+            while(rs.next()){
+                System.out.format("%5s %17s %15s %10s", rs.getString("customer_id"), rs.getString("contact_name"), rs.getString("city"), rs.getString("phone"));
+                System.out.println();
+            }
         } catch (SQLException e) {
-            System.err.println("Connection err" + e) ;
-//            e.printStackTrace();
+            System.err.println("Connection error "+e);
         }
     }
-}
+
+
+    public static void searchCustomersByCustomerName(String customerName) throws ClassNotFoundException {
+        String url = "jdbc:postgresql:northwind";
+        Class.forName("org.postgresql.Driver");
+        try(Connection db = DriverManager.getConnection(url,"christiana-asare","turntabl")) {
+            Statement statement = db.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from customers where contact_name like '%"+customerName+"%';");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.printf("%5s %12s %12s %8s", "Customer_ID", "Contact_Name",  "City", "Telephone");
+            System.out.println();
+            System.out.println("---------------------------------------------------------------------------");
+            while(resultSet.next()){
+                System.out.format("%5s %17s %15s %10s", resultSet.getString("customer_id"), resultSet.getString("contact_name"), resultSet.getString("city"), resultSet.getString("phone"));
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection error "+e);
+        }
+}}
--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -1,21 +1,26 @@
 package io.turntabl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
+        //JDBC connection object
 //              try{
 //                  DBConnection.DBConnection();
 //              } catch (ClassNotFoundException e) {
 //                  e.printStackTrace();
 //              }
-        ProductsDAO products= new ProductDAOImpl();
-        try{
-            for (ProductsTO productSold:products.getProductByCustomerName("Thomas Hardy")){
-                System.out.println(productSold.getProductName());
-                System.out.println(productSold.getUnitPrice());
-                System.out.println();
-            }
+
+
+//        requests a customer name and shows all the customers with similar names.
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter the name of the customer: ");
+            String name = scanner.nextLine();
+            DBConnection.searchCustomersByCustomerName(name);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("Class not found: " + e);
         }
+
     }
 }
